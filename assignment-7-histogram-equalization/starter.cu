446e76a6fb565dec73ad01b2a14c3cdc25de419c
Ragav Subramanian <subramanian.ragav@gmail.com>
2020-11-05 17:09:41 -0500

    Basic cast and grayify implementation

--- assignment-7-histogram-equalization/starter.cu
+++ assignment-7-histogram-equalization/starter.cu
@@ -6,6 +6,47 @@
 //TESTING UPDATES BITCH
 
 //@@ insert code here
+__global__
+unsigned char * cast( float * outputchar, 
+	float * inputfloat, 
+	int imageWidth, 
+	int imageHeight, 
+	int imageChannels)
+{
+	int tidx = (blockIdx.x * blockDim.x) + threadIdx.x; 
+	for (int i = tidx; i < imageWidth * imageHeight * imageChannels, i+= blockDim.x)
+	{
+		outputchar[i] = (unsigned char)(255 * inputfloat[i]);
+	}
+
+	return outputchar;
+}
+
+__global__ 
+void grayify( float * outputgray, 
+	float * inputrgb, 
+	int imageWidth, 
+	int imageHeight, 
+	int imageChannels)
+{
+
+	outputgray = cast(outputgray, inputrgb, imageWidth, imageHeight, imageChannels);
+	
+	synchronize();
+
+	int tidx = (blockIdx.x * blockDim.x) + threadIdx.x; 
+
+	for (int i = tidx; i < imageWidth * imageHeight * imageChannels; i += blockDim.x)
+	{
+		r = outputgray[imageChannels * i];
+		g = outputgray[(imageChannels * i) + 1];
+		b = outputgray[(imageChannels * i) + 1];
+		synchronize();
+		outputgray[tidx] = (unsigned char) (0.21*r + 0.71*g + 0.07*b);
+	}
+
+}
+
 
 int main(int argc, char **argv) {
   wbArg_t args;
@@ -19,6 +60,9 @@ int main(int argc, char **argv) {
   const char *inputImageFile;
 
   //@@ Insert more code here
+  //ANY SETUP IF NEED BE??
+  
+  
 
   args = wbArg_read(argc, argv); /* parse the input arguments */
 
@@ -34,9 +78,35 @@ int main(int argc, char **argv) {
 
   //@@ insert code here
 
+  //get pointers to input and output images
+  hostInputImageData = wbImage_getData(inputImage);
+  hostOutputImageData = wbImage_getData(outputImage);
+
+  //alloc mem and dimensions
+  float * cudaInputImageData, cudaOutputImageData;
+  cudaMalloc(&cudaInputImageData, sizeof(FLOAT) * imageChannels * imageHeight * imageWidth);
+  cudaMemcpy(cudaInputImageData, hostInputImageData, 
+  	sizeof(FLOAT) * imageChannels * imageHeight * imageWidth, cudaMemcpyHostToDevice);
+
+  //send data to kernel
+  grayify<<<256,256>>>(cudaOutputImageData, cudaInputImageData, 
+  	imageWidth, imageHeight, imageChannels);
+
+
+  cudaDeviceSynchronize();
+
+
+  //Retrieve output image data
+  cudaMemcpy(hostOutputImageData, cudaOutputImageData, 
+  	sizeof(FLOAT) * imageChannels * imageHeight * imageWidth, cudaMemcpyDeviceToHost);
+
+
   wbSolution(args, outputImage);
 
   //@@ insert code here
+
+  
+
 
   return 0;
 }
